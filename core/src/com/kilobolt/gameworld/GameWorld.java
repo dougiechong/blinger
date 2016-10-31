a47ccd6ea37a24c7824a37d76af3704494941b2b
Douglas <dougiechong@gmail.com>
2016-10-31 17:14:26 -0400

    added the images

--- core/src/com/kilobolt/gameworld/GameWorld.java
+++ core/src/com/kilobolt/gameworld/GameWorld.java
@@ -1,8 +1,26 @@
 package com.kilobolt.gameworld;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+import com.kilobolt.gameobjects.Bird;
+
 /**
  * Created by douglas on 31/10/16.
  */
 
 public class GameWorld {
+    private Bird bird;
+
+    public GameWorld(int midPointY) {
+        // Initialize bird here
+        bird = new Bird(33, midPointY - 5, 17, 12);
+    }
+
+    public void update(float delta) {
+        bird.update(delta);
+    }
+
+    public Bird getBird() {
+        return bird;
+    }
 }
--- core/src/com/kilobolt/screens/GameScreen.java
+++ core/src/com/kilobolt/screens/GameScreen.java
@@ -2,30 +2,43 @@ package com.kilobolt.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
-/**
- * Created by douglas on 31/10/16.
- */
+import com.kilobolt.gameworld.GameRenderer;
+import com.kilobolt.gameworld.GameWorld;
+import com.kilobolt.zbhelpers.InputHandler;
 
-public class GameScreen implements Screen{
+public class GameScreen implements Screen {
 
+    private GameWorld world;
+    private GameRenderer renderer;
+    private float runTime;
+
+    // This is the constructor, not the class declaration
     public GameScreen() {
-        Gdx.app.log("GameScreen", "Attached");
+
+        float screenWidth = Gdx.graphics.getWidth();
+        float screenHeight = Gdx.graphics.getHeight();
+        float gameWidth = 136;
+        float gameHeight = screenHeight / (screenWidth / gameWidth);
+
+        int midPointY = (int) (gameHeight / 2);
+
+        world = new GameWorld(midPointY);
+        renderer = new GameRenderer(world, (int) gameHeight, midPointY);
+
+        Gdx.input.setInputProcessor(new InputHandler(world.getBird()));
+
     }
 
     @Override
     public void render(float delta) {
-        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
-        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
-        // Fills the screen with the selected color
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        // Covert Frame rate to String, print it
-        Gdx.app.log("GameScreen FPS", (1/delta) + "");
+        runTime += delta;
+        world.update(delta);
+        renderer.render(runTime);
     }
 
     @Override
     public void resize(int width, int height) {
-        Gdx.app.log("GameScreen", "resizing");
+
     }
 
     @Override
@@ -52,4 +65,5 @@ public class GameScreen implements Screen{
     public void dispose() {
         // Leave blank
     }
+
 }
